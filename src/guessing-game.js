6988577fce118a3a0933ba96bcef9d02b02dbb5e
nik25091 <55101647+nik25091@users.noreply.github.com>
2019-10-15 16:19:42 +0300

    feat: complete guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.result = Math.ceil((this.min + this.max) / 2)
+        return this.result
     }
 
     lower() {
-
+        this.max = Math.ceil((this.min + this.max) / 2)
     }
 
     greater() {
-
+        this.min = Math.ceil((this.min + this.max) / 2)
     }
 }
 
